eb592d4cb9362d3ab5265cb0b6738a09a1e3903e
Валерий Дякон <99488340+BananaZ26@users.noreply.github.com>
2025-05-15 23:09:51 +0700

    app.py

--- app.py
+++ app.py
@@ -208,4 +208,6 @@ def profile():
     return render_template('profile.html', user=user, posts=posts)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
